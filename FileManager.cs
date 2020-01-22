751630cb918863c87685adddf7da4a4da1db8afb
JStalnac <55840834+JStalnac@users.noreply.github.com>
2020-01-22 16:01:03 +0200

    First commit

---
+++ FileManager.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace FileSystemManager
+{
+    public class FileManager
+    {
+        /// <summary>
+        /// Loads the files from a directory to a ManagedDirectory.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static ManagedDirectory LoadDirectory(string path) 
+        {
+            if (!Directory.Exists(path)) 
+            {
+                throw new DirectoryNotFoundException($"Unable to load files from '{path}'. That folder does not exist.");
+            }
+
+            List<ManagedFile> files = new List<ManagedFile>();
+
+            foreach (string filename in Directory.EnumerateFiles(path))
+            {
+                ManagedFile managedFile = new ManagedFile(filename);
+                files.Add(managedFile);
+            }
+
+            return new ManagedDirectory(path, files.ToArray());
+        }
+
+        /// <summary>
+        /// Loads the file from the given location.
+        /// </summary>
+        /// <param name="filepath"></param>
+        /// <returns></returns>
+        public static ManagedFile LoadFile(string filepath) 
+        {
+            if (!File.Exists(filepath)) 
+            {
+                throw new FileNotFoundException($"File '{filepath}' not found!");
+            }
+            else
+            {
+                return new ManagedFile(filepath);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FileManager
-A file management system I made for DirtBot
+An actually not that bad file management system I made for [DirtBot](https://github.com/JStalnac/DirtBot).
+
+Ok it's pretty bad. Don't blast me.
